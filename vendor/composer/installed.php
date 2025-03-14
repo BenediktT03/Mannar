864322453bd24b6383dfbbc3b2581186a1477ba0
BenediktT03 <122643262+BenediktT03@users.noreply.github.com>
2025-03-14 01:32:07 +0000

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mannar/website',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6c9d2f243608a0403e96c5ba679d8b6939149c0d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mannar/website' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6c9d2f243608a0403e96c5ba679d8b6939149c0d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
